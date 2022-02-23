190d00549a393254a091eef651e825ff9123f0bc
Eric Tucker <etucker671@gmail.com>
2022-02-23 11:23:38 -0800

    cleaned up fixedstatus data

--- sex_spayed_neutered_separation.R
+++ sex_spayed_neutered_separation.R
@@ -5,6 +5,10 @@ temp <- separate(data[7], col = "SexuponOutcome", into = c("FixedStatus","Sex"),
 temp[temp$FixedStatus=="Spayed",1] <- "Spayed-Neutered"
 temp[temp$FixedStatus=="Neutered",1] <- "Spayed-Neutered"
 
+#convert blanks to Unknown
+temp[temp$FixedStatus=="",1] <- "Unknown"
+temp[is.na(temp$Sex),2] <- "Unknown"
+
 #convert to factors
 temp$FixedStatus <- as.factor(temp$FixedStatus)
 temp$Sex <- as.factor(temp$Sex)
