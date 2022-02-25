5c763dafb6ea8313312bb09c8ac2d1442aae5e00
Eric Tucker <etucker671@gmail.com>
2022-02-24 20:02:28 -0800

    source all file

---
+++ source_all.R
@@ -0,0 +1,6 @@
+#source all scripts
+source("001_setup.R")
+source("002_age_conversion.R")
+source("003_sex_spayed_neutered_separation.R")
+source("004_binary_euth_status.R")
+source("005_month_season_conversion.R")
